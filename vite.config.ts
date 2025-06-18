832fe69c39242d194996f19c0cc4e0233e155c5a
kotatsumuriAZ3 <leplepno03@gmail.com>
2025-06-18 19:19:34 +0900

    取り込み選択後その２

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-=======
   base: '/htmlmatome3/',
->>>>>>> ece416a (first commit読み込みスレッド表示の参照先を変更)
   plugins: [react()],
 })
